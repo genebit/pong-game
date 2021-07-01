54631f53d2850aed686502792fa413105ed9832c
genebit <genebitara@gmail.com>
2021-07-01 14:00:39 +0800

    Added a testing directory

---
+++ test/centerorigin.py
@@ -0,0 +1,24 @@
+
+
+import pygame
+
+pygame.init()
+
+img = pygame.image.load('./test.png')
+
+while True:
+
+    screen = pygame.display.set_mode((300, 300))
+
+    rect = img.get_rect()
+    rect.center = pygame.Vector2(0, 0)
+    screen.blit(img, rect)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: 
+            print('Quit Program')
+            pygame.quit()
+            quit()
+
+    pygame.display.update() 
+
