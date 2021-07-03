bf37149cc869697b6e2cd203f731eaad6c359a4c
genebit <genebitara@gmail.com>
2021-07-03 14:22:21 +0800

    Revised the code, finished implementing movement to the player1 and player2

--- main.py
+++ main.py
@@ -1,74 +1,91 @@
-try:
-    import pygame
-    from pygame import Vector2
-except ImportError as err:
-    print('Import Error occured. \n Error Log: {0}'.format(err))
 
-game_sprites = {
+import pygame
+import sys
+
+from pygame.image import load
+
+GAME_SPRITES = {
     'ball': './sprites/ball/ball.png',
     'board': './sprites/board/board.png',
     'player': './sprites/player/player.png',
     'ui': './sprites/ui/'
 }
 
-class Game:
-    WIDTH = 693  
-    HEIGHT = 364
-    window_size = WIDTH, HEIGHT
+FPS = 60
+WIDTH = 693  
+HEIGHT = 364
+SPEED = 5
 
-    def __init__(self, pygame, sprites):
-        self.pygame = pygame
-        self.sprites = sprites
+class PongGame:
+    
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption('Pong Game')
         
-        # Window properties
-        self.pygame.init()
-        self.pygame.display.set_caption('Pong')
+        WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 
-        # Sprites loaded
-        board_img = self.pygame.image.load(self.sprites['board'])
-        player1_img = self.pygame.image.load(self.sprites['player'])
+        BOARD = pygame.image.load(GAME_SPRITES['board'])
 
-        while True:
-            
-            # Show Game Here...
-            self.screen = self.pygame.display.set_mode(self.window_size)
-            self.show_board(board_img)
-            self.show_player1(player1_img)
-            
-            for event in self.pygame.event.get():
-                # Input Keys...
-                
-                if event.type == self.pygame.KEYDOWN: # This handles all the key pressed input
-                    if event.key == self.pygame.K_w:
-                        print('w pressed')
-                    if event.key == self.pygame.K_s:
-                        print('s pressed')
-                
-                if event.type == self.pygame.KEYUP: # This handles all the key released input
-                    if event.key == self.pygame.K_w:
-                        print('w released')  
-                    if event.key == self.pygame.K_s:
-                        print('s released')
-                        
-                # Quit the program...   
-                self.quit_handler(event)
+        # Player 1 
+        PLAYER1, player1_position = self.player1_properties()
 
-            self.pygame.display.update() 
+        # Player 2
+        PLAYER2, player2_position = self.player2_properties()
 
-    def show_board(self, img):
-        self.screen.blit(img, Vector2(0, 0))
-        
-    def show_player1(self, img):
-        rect = img.get_rect()
-        rect.center = pygame.Vector2(65, self.screen.get_height()/2)
-        self.screen.blit(img, rect)
+        # Game Loop
+        clock = pygame.time.Clock()
+        running = True
+        while running:
+            clock.tick(FPS)
+            for event in pygame.event.get():
+                # Quit Handler
+                if event.type == pygame.QUIT:
+                    running = False
+                    sys.exit()
 
-    def quit_handler(self, event):
-        if event.type == self.pygame.QUIT: 
-            print('Quit Program')
-            self.pygame.quit()
-            quit()
+                # Player 1 Movement Input
+                key_pressed = pygame.key.get_pressed()
+                self.player1_movement_input(player1_position, key_pressed)
+                self.player2_movement_input(player2_position, key_pressed)
+
+            # Draw Objects here...
+            WINDOW.blit(BOARD, (0, 0)) # Draw the Board
+
+            # Draw Player1
+            WINDOW.blit(PLAYER1, player1_position)
+            WINDOW.blit(PLAYER2, player2_position)
+            pygame.display.update()
+
+    def player1_properties(self):
+        PLAYER1 = pygame.image.load(GAME_SPRITES['player'])
+        PLAYER1_START_POS_X = 70
+        PLAYER1_START_POS_y = HEIGHT/2
+
+        player1_position = pygame.image.load(GAME_SPRITES['player']).get_rect()
+        player1_position.center = (PLAYER1_START_POS_X, PLAYER1_START_POS_y)
+        return PLAYER1,player1_position
+
+    def player2_properties(self):
+        PLAYER2 = pygame.image.load(GAME_SPRITES['player'])
+        PLAYER2_START_POS_X = 623
+        PLAYER2_START_POS_y = HEIGHT/2
+
+        player2_position = pygame.image.load(GAME_SPRITES['player']).get_rect()
+        player2_position.center = (PLAYER2_START_POS_X, PLAYER2_START_POS_y)
+        return PLAYER2,player2_position
+
+    def player1_movement_input(self, player1_position, key_pressed):
+        if key_pressed[pygame.K_w]:
+            player1_position.y -= SPEED
+        if key_pressed[pygame.K_s]:
+            player1_position.y += SPEED
+
+    def player2_movement_input(self, player2_position, key_pressed):
+        if key_pressed[pygame.K_o]:
+            player2_position.y -= SPEED
+        if key_pressed[pygame.K_l]:
+            player2_position.y += SPEED
 
 if __name__ == '__main__':
-    Game(pygame, game_sprites)
+    PongGame()
     
