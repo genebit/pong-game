857848b3e67cf5c3e941269eb0299e230d43f78f
genebit <genebitara@gmail.com>
2021-07-03 13:28:45 +0800

    Added movement input from keys

--- test/template.py
+++ test/template.py
@@ -11,14 +11,16 @@ WIDTH = 400
 HEIGHT = 400
 BLACK = (26, 26, 26)
 
+SPEED = 5
+
 class Game:
     def __init__(self, width, height):
         WINDOW = pygame.display.set_mode((width, height))
 
         # Sprite Properties
         BALL = pygame.image.load(assets['ball'])
-        BALL_START_POS_X = 10
-        BALL_START_POS_Y = 10
+        BALL_START_POS_X = WINDOW.get_width()/2
+        BALL_START_POS_Y = WINDOW.get_height()/2
         
         position = pygame.Rect(BALL_START_POS_X, BALL_START_POS_Y, BALL.get_width(), BALL.get_height())
 
@@ -32,6 +34,17 @@ class Game:
                     pygame.quit()
                     quit()
             
+            # Key Input...
+            key_pressed = pygame.key.get_pressed()
+            if key_pressed[pygame.K_d]:
+                position.x += SPEED
+            if key_pressed[pygame.K_a]:
+                position.x -= SPEED
+            if key_pressed[pygame.K_w]:
+                position.y -= SPEED
+            if key_pressed[pygame.K_s]:
+                position.y += SPEED
+                
             # Background
             WINDOW.fill(BLACK)
 
