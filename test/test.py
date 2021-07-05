ba443f59fb53c2e505e670ce50f4e85fbdd8d394
genebit <genebitara@gmail.com>
2021-07-05 16:36:41 +0800

    Added reference

--- test/test.py
+++ test/test.py
@@ -1,30 +1,41 @@
 
 import pygame
 
-FPS = 120
+# TODO: Colliders
 
-WIDTH = 500
-HEIGHT = 500
-GREEN = (95, 255, 89)
+def draw_objects(window):
+    img = pygame.image.load('./test/img/happy.png')
 
-pygame.init()
-CAT_PATH = pygame.image.load('./test/img/happy.png') # Convert it to Surface
+    img_rect = img.get_rect()
+    mouse_x, mouse_y = pygame.mouse.get_pos()
+    img_rect.center = (mouse_x, mouse_y)
 
-WIN = pygame.display.set_mode((WIDTH, HEIGHT)) # Set the Window Size
+    window.blit(img, img_rect)
 
-cat = pygame.Rect(10, 10, 150, 150)
+    RED = (250, 54, 40)
+    pygame.draw.rect(window, RED, img_rect, 2)
 
-
-clock = pygame.time.Clock()
-while True:
-    clock.tick(FPS)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
     
-    # Draw Game Objects here...
-    WIN.fill(GREEN)
-    WIN.blit(CAT_PATH, (cat.x, cat.y))
-    cat.y += 1
-    pygame.display.update()
+def main():
+    
+    WINDOW = pygame.display.set_mode((500, 400))
+
+    clock = pygame.time.Clock()
+    running = True
+    while running:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        # Draw Here
+        BLACK = (26, 26, 26)
+        WINDOW.fill(BLACK)
+
+        draw_objects(WINDOW)
+        pygame.display.update()
+
+if __name__ == '__main__':
+    print('Running Test')
+    main()
