3f1582b14254be3d4e94bd10552d5e03e4ac7a72
genebit <genebitara@gmail.com>
2021-07-09 11:55:19 +0800

    Updated rebounce

--- test/refbounce.py
+++ test/refbounce.py
@@ -8,9 +8,8 @@ speed = [random.randint(5, 15), random.randint(5, 15)]
 
 WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
-ball = pygame.image.load("./img/ball.png")
-ballrect = ball.get_rect()
-print(ballrect)
+ball = pygame.Rect(0, 0, 50, 50)
+
 clock = pygame.time.Clock()
 while True:
     clock.tick(60)
@@ -18,15 +17,15 @@ while True:
         if event.type == pygame.QUIT:
             sys.exit()
 
-    ballrect = ballrect.move(speed)
-
-    if ballrect.left < 0 or ballrect.right > WINDOW_WIDTH: # Checks if the collider hits the window border
+    ball = ball.move(speed)
+    
+    if ball.left < 0 or ball.right > WINDOW_WIDTH: # Checks if the collider hits the window border
         speed[0] = -speed[0] # Converts the speed to move negative to bounce back
 
-    if ballrect.top < 0 or ballrect.bottom > WINDOW_HEIGHT:
+    if ball.top < 0 or ball.bottom > WINDOW_HEIGHT:
         speed[1] = -speed[1]
 
     WINDOW.fill((26, 26, 26))
-    WINDOW.blit(ball, ballrect)
+    pygame.draw.rect(WINDOW, (255, 255, 255), ball)
     
     pygame.display.update()
