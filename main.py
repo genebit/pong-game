ad624c1539ae81ed52d89e5cab94a470d3622871
genebit <genebitara@gmail.com>
2021-07-09 11:25:58 +0800

    Minor Changes

--- main.py
+++ main.py
@@ -16,46 +16,40 @@ SPRITES = {
     "board": "./sprites/board/board.png",
 }
 
-class PongGame:
+FPS = 60
+WINDOW_WIDTH, WINDOW_HEIGHT = 600, 400 
 
-    def __init__(self, width, height, fps):
-        pygame.init()
+pygame.init()
 
-        # Set the Window Properties
-        self.WINDOW = pygame.display.set_mode((width, height))
-        pygame.display.set_caption('Classic Pong Game')
+# Load Images...
+BOARD = pygame.image.load(SPRITES['board'])
+PLAYER1 = pygame.image.load(SPRITES['player'])
 
-        # Load the Images
-        board = pygame.image.load(SPRITES['board'])
-        self.player1 = pygame.image.load(SPRITES['player'])
-        self.player2 = pygame.image.load(SPRITES['player'])
-        self.ball = pygame.image.load(SPRITES['ball'])
-        
-        # Game Loop
-        running = True
+def draw_objects(window):
+    window.blit(BOARD, (0, 0))
 
-        clock = pygame.time.Clock()
-        while running:
-            clock.tick(fps)
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            
-            # Draw Here
-            self.WINDOW.blit(board, (0, 0))
-            
-            self.player1.center = (50, self.WINDOW.get_height()/2)
-
-            pygame.display.update()
+    PLAYER1_RECT = PLAYER1.get_rect()
+    PLAYER1_RECT.center = (60, window.get_height()/2)
     
-    def player1_properties(self, position_x, position_y, speed):
-        self.player1 = pygame.get_rect()
-        self.player1.center = (50, self.WINDOW.get_height()/2)
-        
-        
+    window.blit(PLAYER1, PLAYER1_RECT)
+
+def main():
+    # Where The main loop, main properties...
+    WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    pygame.display.set_caption('Pong Game')
+    
+    # Main Loop...
+    clock = pygame.time.Clock()
+    while True:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: # Quit Handler...
+                sys.exit()
+
+        # Draw Here...
+        draw_objects(WINDOW)
+
+        pygame.display.update()
+
 if __name__ == '__main__':
-    FPS = 60
-    WINDOW_WIDTH = 600
-    WINDOW_HEIGHT = 400
-    
-    PongGame(WINDOW_WIDTH, WINDOW_HEIGHT, FPS)
+    main()
