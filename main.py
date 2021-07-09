4957a12850ab53fe1b29dcc3da1b51aab7194107
genebit <genebitara@gmail.com>
2021-07-09 14:26:52 +0800

    Minor refactoring

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import random
 import time
 
 FPS = 60
-WINDOW_WIDTH, WINDOW_HEIGHT = 600, 300 
+WINDOW_WIDTH, WINDOW_HEIGHT = (600, 300 )
+BLACK = (20, 20, 20)
+WHITE = (255, 255, 255)
 
 PLAYER_SPEED = 5
 P1_VELOCITY = [0, PLAYER_SPEED]
-BALL_SPEED = [random.randint(5, 10), random.randint(5, 10)]
+BALL_SPEED = [8, 8]
 
 pygame.init()
 
@@ -18,35 +20,39 @@ pygame.display.set_caption('Pong Game')
 
 # Load Images...
 PLAYER1 = pygame.Rect(0, 0, 15, 50)
-PLAYER1.center = (50, WINDOW.get_height()/2)
+PLAYER1.center = (50, WINDOW_HEIGHT/2)
 
 PLAYER2 = pygame.Rect(0, 0, 15, 50)
-PLAYER2.center = (550, WINDOW.get_height()/2)
+PLAYER2.center = (550, WINDOW_HEIGHT/2)
 
 BALL = pygame.Rect(0, 0, 15, 15)
-BALL.center = (WINDOW.get_width()/2, random.randint(30, 370))
+BALL.center = (WINDOW_WIDTH/2, random.randint(30, 370))
 
 # Main Loop...
 clock = pygame.time.Clock()
 
-def p1_movement_input(window, player1):
+def p1_movement_input(player1):
     global PLAYER_SPEED, P1_VELOCITY
+    
     keypressed = pygame.key.get_pressed()
+
     if keypressed[pygame.K_w] and not keypressed[pygame.K_s]:
         P1_VELOCITY = [0, -PLAYER_SPEED] if player1.top > 0 else [0, 0]
     elif keypressed[pygame.K_s] and not keypressed[pygame.K_w]:
-        P1_VELOCITY = [0, PLAYER_SPEED] if player1.bottom < window.get_height() else [0, 0]        
+        P1_VELOCITY = [0, PLAYER_SPEED] if player1.bottom < WINDOW_HEIGHT else [0, 0]        
     else:
         P1_VELOCITY = [0, 0]
     return P1_VELOCITY
 
-def p2_movement_input(window, player2):
+def p2_movement_input(player2):
     global PLAYER_SPEED, P1_VELOCITY
+
     keypressed = pygame.key.get_pressed()
+
     if keypressed[pygame.K_UP] and not keypressed[pygame.K_DOWN]:
         P2_VELOCITY = [0, -PLAYER_SPEED] if player2.top > 0 else [0, 0]
     elif keypressed[pygame.K_DOWN] and not keypressed[pygame.K_UP]:
-        P2_VELOCITY = [0, PLAYER_SPEED] if player2.bottom < window.get_height() else [0, 0]        
+        P2_VELOCITY = [0, PLAYER_SPEED] if player2.bottom < WINDOW_HEIGHT else [0, 0]        
     else:
         P2_VELOCITY = [0, 0]
     return P2_VELOCITY
@@ -58,14 +64,7 @@ while True:
             sys.exit()
 
     # Draw Here...
-    BLACK = (20, 20, 20)
-    WHITE = (255, 255, 255)
-
     WINDOW.fill(BLACK)
-    
-    # Players
-    P1_VELOCITY = p1_movement_input(WINDOW, PLAYER1)
-    P2_VELOCITY = p2_movement_input(WINDOW, PLAYER2)
     
     # region ball
     BALL = BALL.move(BALL_SPEED)
@@ -80,18 +79,24 @@ while True:
 
     # Verdict
     if BALL.left < 0:
-        BALL.center = (WINDOW.get_width()/2, random.randint(20, 380))
+        BALL.center = (WINDOW_WIDTH/2, random.randint(20, 380))
         time.sleep(1)
+
     elif BALL.right > WINDOW_WIDTH:
-        BALL.center = (WINDOW.get_width()/2, random.randint(20, 380))
+        BALL.center = (WINDOW_WIDTH/2, random.randint(20, 380))
         time.sleep(1)
-    pygame.draw.rect(WINDOW, WHITE, BALL)
+        
+    pygame.draw.rect(WINDOW, WHITE, BALL, 3)
     # endregion
 
+    # Players
+    P1_VELOCITY = p1_movement_input(PLAYER1)
+    P2_VELOCITY = p2_movement_input(PLAYER2)
+    
     PLAYER1 = PLAYER1.move(P1_VELOCITY)
-    pygame.draw.rect(WINDOW, WHITE, PLAYER1)
+    pygame.draw.rect(WINDOW, WHITE, PLAYER1, 3)
 
     PLAYER2 = PLAYER2.move(P2_VELOCITY)
-    pygame.draw.rect(WINDOW, WHITE, PLAYER2)
+    pygame.draw.rect(WINDOW, WHITE, PLAYER2, 3)
 
     pygame.display.update()
